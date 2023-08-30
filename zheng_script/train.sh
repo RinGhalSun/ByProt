fd922b43251015b7acd35959711e337a381d739c
Z-MU-Z <2391652462@qq.com>
2023-08-31 06:28:23 +0800

    add CATH path

--- zheng_script/train.sh
+++ zheng_script/train.sh
@@ -1,12 +1,9 @@
-HYDRA_FULL_ERROR=1
 export CUDA_VISIBLE_DEVICES=0
-# or use multi-gpu training when you want:
-# export CUDA_VISIBLE_DEVICES=0,1
 
-exp=fixedbb/protein_mpnn_cmlm  
+exp=fixedbb/lm_design_esm1b_650m
 dataset=cath_4.2
-name=fixedbb/${dataset}/protein_mpnn_cmlm
+name=fixedbb/${dataset}/lm_design_esm1b_650m
 
 python ./train.py \
     experiment=${exp} datamodule=${dataset} name=${name} \
-    logger=tensorboard trainer=ddp_fp16
+    logger=tensorboard trainer=ddp_fp16 
